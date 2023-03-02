f5706ab9c6dc72060fda4d06babe0585609b3bb1
ashishingle29 <ashishingle589@gmail.com>
2023-03-02 19:48:25 +0530

    All Done

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+const http = require("http").Server(app);
+const cors = require("cors");
+const { users } = require("./lib/users");
+const { listComments, createComment } = require("./lib/comments");
+
+const io = require("socket.io")(http, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+
+
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+io.on("connection", (socket) => {
+  // console.log(socket)
+    console.log(`⚡: ${socket.id} user just connected!`);
+  
+    socket.on("disconnect", () => {
+      console.log("🔥: A user disconnected");
+    });
+  });
+
+
+app.get("/users", (req, res) => {
+    res.json(users);
+  });
+
+  app.get("/comments", (req, res) => {
+    const comments = listComments();
+    res.json(comments);
+  });
+  app.post("/comments", async (req, res) => {
+    const comment = createComment(req.body);
+    io.emit("new-comment", { comment });
+    res.json(comment);
+  });
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
